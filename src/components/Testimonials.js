11f5b0864680c23e61822e66ccfba3fd14cecc40
Tlamaus <honza.moravec@starkysclub.com>
2019-08-13 17:20:10 +0200

    styledComponents test no. 4

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
 const Testimonials = ({ testimonials }) => (
   <div className="Testimonials">
