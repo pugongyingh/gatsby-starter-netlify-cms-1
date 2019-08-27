8fd1bf4d21e93b453849d429e89c87d2ed0eb3b2
Hai Ha Nguyen <haiha.nguyen@starkysclub.com>
2019-08-27 14:11:52 +0200

    adding necessery fonts and basic styling

--- src/styles/main.tsx
+++ src/styles/main.tsx
@@ -1,8 +1,26 @@
 import { css } from "./theme";
 
 export const main = css`
-    body {
-        margin:0;
-        padding: 0;
+  /*Global styles*/
+  body {
+    margin: 0;
+    padding: 0;
+    @import url("https://fonts.googleapis.com/css?family=Muli|Open+Sans&display=swap");
+    font-family: "Muli", sans-serif;
+  }
+
+  .container-fluid {
+    padding: 0 !important;
+  }
+
+  /*All Main Sections*/
+  .what-we-do {
+    color: ${props => props.theme.colors.white};
+    background-color: ${props => props.theme.colors.darkGreen};
+    padding: 2rem 0;
+
+    @media ${props => props.theme.screen.laptop} {
+      padding: 7rem 0;
     }
+  }
 `;
