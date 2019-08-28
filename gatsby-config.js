95f1d29f666bfb123086a1dc0b7b7f6c4e0ff456
Václav Brzezina <vaclav.brzezina@starkysclub.com>
2019-08-28 11:01:29 +0200

    add source

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,13 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
+        path: `${__dirname}/src/config`,
+        name: "config"
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
         path: `${__dirname}/src/img`,
         name: "images"
       }
