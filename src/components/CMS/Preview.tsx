8e6ccce0cee08882e6067b8af66f2fdcfe1dbda6
Václav Brzezina <vaclav.brzezina@starkysclub.com>
2019-08-30 11:10:51 +0200

    styles

--- src/components/CMS/Preview.tsx
+++ src/components/CMS/Preview.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import { theme, ThemeProvider } from "../../styles/theme";
+import { main } from '../../styles/main';
+import { css, theme, ThemeProvider } from "../../styles/theme";
 
 export const Preview: React.SFC = ({ children }) => {
     return (
         <ThemeProvider theme={theme}>
-            {children}
+            <React.Fragment>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:400,700|Open+Sans:400,700,800&display=swap" />
+                <style>{css`${main}`}</style>
+                {children}
+            </React.Fragment>
         </ThemeProvider >
     );
 }
