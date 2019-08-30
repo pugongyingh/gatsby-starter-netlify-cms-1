b7a3f1acdc5858e341aba299f952a0bc835e30b3
Hai Ha Nguyen <haiha.nguyen@starkysclub.com>
2019-08-30 15:53:37 +0200

    adding new font

--- src/components/CMS/Preview.tsx
+++ src/components/CMS/Preview.tsx
@@ -1,15 +1,24 @@
-import * as React from 'react';
-import { main } from '../../styles/main';
+import * as React from "react";
+import { main } from "../../styles/main";
 import { css, theme, ThemeProvider } from "../../styles/theme";
 
 export const Preview: React.SFC = ({ children }) => {
-    return (
-        <ThemeProvider theme={theme}>
-            <React.Fragment>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:400,700|Open+Sans:400,700,800&display=swap" />
-                <style>{css`${main}`}</style>
-                {children}
-            </React.Fragment>
-        </ThemeProvider >
-    );
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Muli:400,700|Open+Sans:400,700,800&display=swap"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap"
+          rel="stylesheet"
+        />
+        <style>{css`
+          ${main}
+        `}</style>
+        {children}
+      </React.Fragment>
+    </ThemeProvider>
+  );
+};
