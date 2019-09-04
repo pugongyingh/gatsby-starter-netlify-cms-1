96e31d5c808dded8dd5e909f127affcdfd037241
Honza Moravec <Honza.moravec@starkysclub.com>
2019-09-04 16:07:41 +0200

    Conflict resolve (once and for all!)

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -16,7 +16,7 @@ const Image: React.FC<ImageProps> = props => {
   const { className, source, altText } = props;
   return (
     <div>
-      <ResponsiveImg className={className} src={source} alt={altText} />;
+      <ResponsiveImg className={className} src={source} alt={altText} />
     </div>
   );
 };
