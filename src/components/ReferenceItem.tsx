06f575a2f5af59946267b81aba36e2916adde30a
Hai Ha Nguyen <haiha.nguyen@starkysclub.com>
2019-09-10 17:42:16 +0200

    adding a ArrowLink comp

--- src/components/ReferenceItem.tsx
+++ src/components/ReferenceItem.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Col, Row } from "react-flexbox-grid";
+import ArrowLink from "../components/Carousel/ArrowLink";
 import Image from "../components/Image";
 
 interface Props {
-    src: string;
-    alt: string;
-    heading: string;
-    subheading: string;
-    perex: string;
+  src: string;
+  alt: string;
+  heading: string;
+  subheading: string;
+  perex: string;
+  link: string;
 }
 
-const ReferenceItem: React.FC<Props> = ( {src, alt, heading, subheading, perex }) => {
+const ReferenceItem: React.FC<Props> = ({
+  src,
+  alt,
+  heading,
+  subheading,
+  perex,
+  link
+}) => {
   return (
     <Row className="section-row">
       <Col xs={12} sm={6} lg={5}>
@@ -19,9 +28,8 @@ const ReferenceItem: React.FC<Props> = ( {src, alt, heading, subheading, perex }
       <Col className="ref-content" xs={12} sm={6} lgOffset={1} lg={6}>
         <h2>{heading}</h2>
         <h3>{subheading}</h3>
-        <p>
-          {perex}
-        </p>
+        <p>{perex}</p>
+        <ArrowLink link={link} text="See live site" instaFollowLink={true}/>
       </Col>
     </Row>
   );
