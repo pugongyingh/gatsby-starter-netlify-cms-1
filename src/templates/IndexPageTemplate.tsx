5488564a39de0e91bbf0486d9c09a5dde84ec4ef
Václav Brzezina <vaclav.brzezina@starkysclub.com>
2019-09-12 13:26:46 +0200

    small adjusment

--- src/templates/IndexPageTemplate.tsx
+++ src/templates/IndexPageTemplate.tsx
@@ -171,6 +171,7 @@ export const StyledIndexPageTemplate = styled(IndexPageTemplate)`
   .container {
     @media ${props => props.theme.screen.laptopL} {
       width: 1410px !important;
+      max-width: 1410px !important;
     }
   }
 
