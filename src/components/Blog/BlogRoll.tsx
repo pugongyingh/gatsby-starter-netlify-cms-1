3abe871e88c3c403f6531242f70d05dc45b332eb
Jan Moravec <moravec2jan@gmail.com>
2019-09-12 15:35:47 +0200

    carousel responsive

--- src/components/Blog/BlogRoll.tsx
+++ src/components/Blog/BlogRoll.tsx
@@ -1,65 +1,67 @@
-import { graphql, StaticQuery } from "gatsby";
-import React from "react";
-import { Col, Grid, Row } from "react-styled-flexboxgrid";
-import { MarkdownRemark, Maybe, Query } from "../../graphql/types";
-import styled, { SCP } from "../../styles/theme";
-import StyledCarousel from "../Carousel/Carousel";
-import StyledBlogItem from "./BlogItem";
+import { graphql, StaticQuery } from 'gatsby';
+import React from 'react';
+import { Col, Grid, Row } from 'react-styled-flexboxgrid';
+import { MarkdownRemark, Maybe, Query } from '../../graphql/types';
+import styled, { SCP } from '../../styles/theme';
+import StyledCarousel from '../Carousel/Carousel';
+import StyledBlogItem from './BlogItem';
 
 interface P extends SCP {
-  blogPosts: MarkdownRemark[];
-  locale: Maybe<string>;
+	blogPosts: MarkdownRemark[];
+	locale: Maybe<string>;
 }
 
 class Blog extends React.Component<P> {
-  public render() {
-    const blogPosts = this.props.blogPosts && this.props.blogPosts.filter(post => post.frontmatter!.locale === this.props.locale);
-    if (!blogPosts.length) {
-      return null;
-    }
-    return (
-      <section className={this.props.className}>
-        <Grid className="container">
-          <h1>Blog</h1>
-          <Row className="blog-wrap">
-            <Col className="blog-item">
-              <StyledCarousel>
-                {blogPosts.map((post: MarkdownRemark) => {
-                  return (
-                    <StyledBlogItem
-                      className="block-item"
-                      key={post.id}
-                      image={post.frontmatter!.image!}
-                      title={post.frontmatter!.title!}
-                      description={post.frontmatter!.description!}
-                    />
-                  )
-                })}
-              </StyledCarousel>
-            </Col>
-          </Row>
-        </Grid>
-      </section>
-    );
-  }
+	public render() {
+		const blogPosts =
+			this.props.blogPosts &&
+			this.props.blogPosts.filter((post) => post.frontmatter!.locale === this.props.locale);
+		if (!blogPosts.length) {
+			return null;
+		}
+		return (
+			<section className={this.props.className}>
+				<Grid className="container">
+					<h1>Blog</h1>
+					<Row className="blog-wrap">
+						<Col className="blog-item">
+							<StyledCarousel>
+								{blogPosts.map((post: MarkdownRemark) => {
+									return (
+										<StyledBlogItem
+											className="block-item"
+											key={post.id}
+											image={post.frontmatter!.image!}
+											title={post.frontmatter!.title!}
+											description={post.frontmatter!.description!}
+										/>
+									);
+								})}
+							</StyledCarousel>
+						</Col>
+					</Row>
+				</Grid>
+			</section>
+		);
+	}
 }
 
 const StyledBlog = styled(Blog)`
-  background-color: ${props => props.theme.colors.blogSectionBg};
-  color: ${props => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors.blogSectionBg};
+  color: ${(props) => props.theme.colors.white};
   padding: 2rem 0;
 
-  @media ${props => props.theme.screen.laptop} {
+  @media ${(props) => props.theme.screen.laptop} {
     padding: 7.5rem 0;
   }
 
   .row {
-    @media ${props => props.theme.screen.laptop} {
+    @media ${(props) => props.theme.screen.laptop} {
       margin-top: 95px;
     }
   }
   .slick-slider {
-    margin-right: -6rem;
+    margin-right: -4rem;
   }
   .blog-item {
     width: 100%;
@@ -72,7 +74,7 @@ const StyledBlog = styled(Blog)`
     flex-wrap: wrap;
     justify-content: space-between;
 
-    @media ${props => props.theme.screen.laptop} {
+    @media ${(props) => props.theme.screen.laptop} {
       margin-top: 100px;
 
       h2 {
@@ -80,26 +82,35 @@ const StyledBlog = styled(Blog)`
       }
     }
   }
+  @media ${(props) => props.theme.screen.mobileL} {
+    .slick-slider {
+    margin-right: 0;
+    }
+  }
 `;
 
-
-const BlogRollQuery: React.SFC<Omit<P, "blogPosts">> = props => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query BlogRollQuery {
-          allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "BlogPost"}}}, sort: {order: DESC, fields: frontmatter___date}) {
-            edges {
-              node {
-                  ...BlogInfo
-              }
-            }
-          }
-        }
-      `}
-      render={(blogPosts: Query) => <StyledBlog blogPosts={blogPosts.allMarkdownRemark.edges.map((edge) => edge.node)} {...props} />}
-    />
-  );
+const BlogRollQuery: React.SFC<Omit<P, 'blogPosts'>> = (props) => {
+	return (
+		<StaticQuery
+			query={graphql`
+				query BlogRollQuery {
+					allMarkdownRemark(
+						filter: { frontmatter: { templateKey: { eq: "BlogPost" } } }
+						sort: { order: DESC, fields: frontmatter___date }
+					) {
+						edges {
+							node {
+								...BlogInfo
+							}
+						}
+					}
+				}
+			`}
+			render={(blogPosts: Query) => (
+				<StyledBlog blogPosts={blogPosts.allMarkdownRemark.edges.map((edge) => edge.node)} {...props} />
+			)}
+		/>
+	);
 };
 
 export default BlogRollQuery;
--- src/components/Instagram/InstagramRoll.tsx
+++ src/components/Instagram/InstagramRoll.tsx
@@ -1,50 +1,47 @@
-import { graphql, StaticQuery } from "gatsby";
-import React from "react";
-import { Col, Grid, Row } from "react-styled-flexboxgrid";
-import { InstaNode, Query } from "../../graphql/types";
-import styled, { SCP } from "../../styles/theme";
-import ArrowLink from "../Carousel/ArrowLink";
-import StyledCarousel from "../Carousel/Carousel";
-import StyledInstagramItem from "./InstagramItem";
+import { graphql, StaticQuery } from 'gatsby';
+import React from 'react';
+import { Col, Grid, Row } from 'react-styled-flexboxgrid';
+import { InstaNode, Query } from '../../graphql/types';
+import styled, { SCP } from '../../styles/theme';
+import ArrowLink from '../Carousel/ArrowLink';
+import StyledCarousel from '../Carousel/Carousel';
+import StyledInstagramItem from './InstagramItem';
 
 interface P extends SCP {
-  instagramPosts: InstaNode[];
+	instagramPosts: InstaNode[];
 }
 
 class Instagram extends React.Component<P> {
-  public render() {
-    return (
-      <section className={this.props.className}>
-        <Grid className="container">
-          <h1>Instagram</h1>
-          <Row className="instagram-items-wrap">
-            <Col className="instagram-item">
-              <StyledCarousel dark={true}>
-                {this.props.instagramPosts && this.props.instagramPosts.map((post: InstaNode) => {
-                  return (
-                    <StyledInstagramItem
-                      key={post.id}
-                      src={post.original!}
-                      alt={post.caption!}
-                      instaText={post.caption!}
-                    />
-                  )
-                })}
-              </StyledCarousel>
-            </Col>
-          </Row>
-          <Row />
-          <Row className="arrow-icons-wrap">
-            <ArrowLink
-              text="Follow us on Instagram"
-              link="#"
-              instaFollowLink={true}
-            />
-          </Row>
-        </Grid>
-      </section>
-    );
-  }
+	public render() {
+		return (
+			<section className={this.props.className}>
+				<Grid className="container">
+					<h1>Instagram</h1>
+					<Row className="instagram-items-wrap">
+						<Col className="instagram-item">
+							<StyledCarousel dark={true}>
+								{this.props.instagramPosts &&
+									this.props.instagramPosts.map((post: InstaNode) => {
+										return (
+											<StyledInstagramItem
+												key={post.id}
+												src={post.original!}
+												alt={post.caption!}
+												instaText={post.caption!}
+											/>
+										);
+									})}
+							</StyledCarousel>
+						</Col>
+					</Row>
+					<Row />
+					<Row className="arrow-icons-wrap">
+						<ArrowLink text="Follow us on Instagram" link="#" instaFollowLink={true} />
+					</Row>
+				</Grid>
+			</section>
+		);
+	}
 }
 
 const StyledInstagram = styled(Instagram)`
@@ -56,14 +53,19 @@ const StyledInstagram = styled(Instagram)`
   align-items: center;
 
   .slick-slider {
-    margin-right: -6rem;
+    margin-right: -2rem;
   }
   .instagram-item {
     width: 100%;
     overflow: hidden;
   }
+  @media ${(props) => props.theme.screen.mobileS} {
+    .slick-slider {
+      margin-right: 0;
+    }
+  }
 
-  @media ${props => props.theme.screen.laptop} {
+  @media ${(props) => props.theme.screen.laptop} {
     .slider {
       margin-right: 2px;
     }
@@ -81,26 +83,31 @@ const StyledInstagram = styled(Instagram)`
     margin-right: auto;
   }
 }
+
 `;
 
-
-const InstagramRoll: React.SFC<Omit<P, "instagramPosts">> = props => {
-  return (
-    <StaticQuery
-      query={graphql`
-              query InstagramRollQuery {
-                allInstaNode(sort: {order: DESC, fields: timestamp}) {
-                  edges {
-                    node {
-                      ...InstaNodeInfo
-                    }
-                  }
-                }
-              }
-            `}
-      render={(instagramPosts: Query) => <StyledInstagram instagramPosts={instagramPosts.allInstaNode.edges.map((edge) => edge.node)} {...props} />}
-    />
-  );
+const InstagramRoll: React.SFC<Omit<P, 'instagramPosts'>> = (props) => {
+	return (
+		<StaticQuery
+			query={graphql`
+				query InstagramRollQuery {
+					allInstaNode(sort: { order: DESC, fields: timestamp }) {
+						edges {
+							node {
+								...InstaNodeInfo
+							}
+						}
+					}
+				}
+			`}
+			render={(instagramPosts: Query) => (
+				<StyledInstagram
+					instagramPosts={instagramPosts.allInstaNode.edges.map((edge) => edge.node)}
+					{...props}
+				/>
+			)}
+		/>
+	);
 };
 
 export default InstagramRoll;
