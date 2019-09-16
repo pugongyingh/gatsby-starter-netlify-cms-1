293e02d5708c0438e319a24f21624c25827b03db
Hai Ha Nguyen <haiha.nguyen@starkysclub.com>
2019-09-16 13:33:47 +0200

    styling

--- src/components/Contact/ContactCard.tsx
+++ src/components/Contact/ContactCard.tsx
@@ -45,7 +45,11 @@ const ContactCardWrap = styled.div`
   .linkedin-icon {
     margin-left: 10px;
 
-    @media ${props => props.theme.screen.laptop} {
+    @media ${props => props.theme.screen.tablet} {
+      margin-left: 30px;
+    }
+
+    @media ${props => props.theme.screen.laptopL} {
       margin-left: 40px;
     }
   }
