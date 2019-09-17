537435a4f09117b4f84efc83d98deeae0b9bd5cf
Hai Ha Nguyen <haiha.nguyen@starkysclub.com>
2019-09-17 16:33:24 +0200

    optimized breakpoint

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -132,7 +132,7 @@ export const theme: IThemeInterface = {
   screen: {
     mobileS: "(min-width: 320px)",
     mobileM: "(min-width: 375px)",
-    mobileL: "(min-width: 425px)",
+    mobileL: "(min-width: 579px)",
     tablet: "(min-width: 768px)",
     laptop: "(min-width: 1024px)",
     laptopL: "(min-width: 1440px)",
