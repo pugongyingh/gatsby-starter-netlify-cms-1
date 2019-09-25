ce6175eeb3ddbf99b0feb91e3b64ad4e5c90f2de
Hai Ha Nguyen <haiha.nguyen@starkysclub.com>
2019-09-25 10:31:23 +0200

    adding back the basic jsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,14 +8,12 @@ interface NotFoundPageProps {
 }
 
 const NotFoundPage: React.SFC<NotFoundPageProps> = ({hero}) => (
-
-  // <div>
-  //   <div>
-  //     <h1>NOT FOUND</h1>
-  //     <p>You just hit a route that doesn&#39;t exist... the sadness. </p>
-  //   </div>
-  // </div>
-  <Header title="404" subheading="You just hit a route that doesn't exist... the sadness." hero={hero} locale={locale} />
+  <div>
+    <div>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness. </p>
+    </div>
+  </div>
 );
 
 export default NotFoundPage;
