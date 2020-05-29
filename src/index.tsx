5126e9e0e3105a504f54aa0ce9e6b2bfb3e5c735
pugongyingh <pgyhh@sina.cn>
2020-05-29 17:35:03 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,14 @@
+
+import CMS from "netlify-cms-app";
+import cloudinary from "netlify-cms-media-library-cloudinary";
+import uploadcare from "netlify-cms-media-library-uploadcare";
+import * as React from 'react';
+import { StyleSheetManager } from 'styled-components';
+import { BlogPostPreview } from "./pages/BlogPost";
+import { IndexPagePreview } from "./pages/IndexPage";
+
+
+
 import React from 'react';
 import {
     Layout,
@@ -25,6 +36,44 @@ import styled from '@emotion/styled';
 import { useUser } from './hooks/useUser';
 import { useLevels } from './hooks/useLevels';
 import { useSubscriptions } from './hooks/useSubscriptions';
+
+
+
+const CSSInjector = (props: any) => {
+    const iframe = document.getElementsByTagName('iframe')[0];
+    if (iframe) {
+        return (
+            <StyleSheetManager target={iframe.contentDocument!.head}>
+                {props.children}
+            </StyleSheetManager>
+        )
+    }
+    return null;
+}
+
+CMS.registerPreviewTemplate('index-cs', (props: any) => (
+    <CSSInjector>
+        <IndexPagePreview {...props} />
+    </CSSInjector>
+))
+
+CMS.registerPreviewTemplate('index-en', (props: any) => (
+    <CSSInjector>
+        <IndexPagePreview {...props} />
+    </CSSInjector>
+))
+
+CMS.registerPreviewTemplate('blog', (props: any) => (
+    <CSSInjector>
+        <BlogPostPreview {...props} />
+    </CSSInjector>
+))
+
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
+
+
+
 
 const { Header, Content, Footer } = Layout;
 
